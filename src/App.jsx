e3bbc790d3af886dacacc362d3ee9f32d7acb187
Philip-Machar <philipmachar788@gmail.com>
2024-08-18 19:31:18 +0300

    Complete routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobsListings from './components/JobsListings';
-import ViewAllJobs from './components/ViewAllJobs';
+
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route index element={<h1>Hello World</h1>} />
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>
   )
 );
 
