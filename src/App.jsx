9e13e7262a5c8c29ddf47950d3f99a76e1cd1274
Philip-Machar <philipmachar788@gmail.com>
2024-08-18 09:58:00 +0300

    useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import HomeCards from './components/HomeCards';
 import JobsListings from './components/JobsListings';
 import ViewAllJobs from './components/ViewAllJobs';
 
-const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobsListings />
-      <ViewAllJobs />
-    </>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route index element={<h1>Hello World</h1>} />
   )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />
 }
 
 export default App
